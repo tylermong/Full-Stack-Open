0547aa24f1f09be61136d309ae1508c1862d05a6
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 13:17:44 -0500

    Refactor App component by adding Header, Content, and Total components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,7 @@
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +13,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total value1={exercises1} value2={exercises2} value3={exercises3} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,7 @@
+const Total = (props) => {
+    return (
+        <p>Number of exercises { props.value1 + props.value2 + props.value3 }</p>
+    )
+}
+
+export default Total
