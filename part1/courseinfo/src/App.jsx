e6f80938aac8f1b4db84505563fa48123a9a6418
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 14:00:04 -0500

    Refactor App, Content, Part, and Total components to use an array for parts

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,26 +4,26 @@ import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} />
-      <Content part={part2} />
-      <Content part={part3} />
-      <Total value1={part1.exercises} value2={part2.exercises} value3={part3.exercises}  />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,6 +1,6 @@
 const Total = (props) => {
     return (
-        <p>Number of exercises { props.value1 + props.value2 + props.value3 }</p>
+        <p>Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
     )
 }
 
