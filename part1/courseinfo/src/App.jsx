9a9f5bc39fe8d408041e47e5ce15016d98b2a691
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 13:52:25 -0500

    Refactor App and Content components to use individual part objects instead of an array

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,26 +4,26 @@ import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {
-      part: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      part: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total value1={parts[0].exercises} value2={parts[1].exercises} value3={parts[2].exercises} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total value1={part1.exercises} value2={part2.exercises} value3={part3.exercises}  />
     </div>
   )
 }
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,7 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part part={props.parts[0].part} exercise={props.parts[0].exercises} />
-            <Part part={props.parts[1].part} exercise={props.parts[1].exercises} />
-            <Part part={props.parts[2].part} exercise={props.parts[2].exercises} />
+            <Part part={props.part.name} exercise={props.part.exercises} />
         </>
     )
 }
