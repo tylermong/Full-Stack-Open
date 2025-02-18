45d0a6467ee05db20c8d0ac8a2d7033b96a534fd
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 14:08:20 -0500

    Refactor App, Content, Header, and Total components to use course object for parts

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+            <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
+            <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+            <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
         </>
     )
 }
