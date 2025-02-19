323be5cc6bcabe784d508cf2e13b7455adcf0112
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 20:53:06 -0500

    Refactor App component to manage counter state internally and remove manual refresh logic

--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -2,17 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-  }, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
