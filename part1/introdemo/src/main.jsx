9eeb98f87f5a3072e22f6966453125945a7b4b39
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 20:46:04 -0500

    Refactor App component to accept counter prop and update rendering logic

--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -2,4 +2,18 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+  root.render(
+    <App counter={counter} />
+  )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
