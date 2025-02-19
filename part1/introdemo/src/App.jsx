2bf1f572318352b9d822cdd2da452c1395d01582
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-18 21:00:26 -0500

    Add buttons to increment and reset counter in App component

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   console.log('rendering...', counter)
 
   return (
-    <div>{counter}</div>
+    <>
+        <div>{counter}</div>
+        {/* <button onClick={handleClick}>plus</button> */}
+        <button onClick={() => setCounter(counter + 1)}>
+            plus
+        </button>
+        <button onClick={() => setCounter(0)}>
+            zero
+        </button>
+    </>
   )
 }
 
