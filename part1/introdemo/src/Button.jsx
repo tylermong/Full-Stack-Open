f62d04082a45dd89c990b69b6ac83845da914776
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-03-10 15:10:23 -0400

    Refactor Button component to use destructuring for props

--- part1/introdemo/src/Button.jsx
+++ part1/introdemo/src/Button.jsx
@@ -1,7 +1,7 @@
-const Button = (props) => {
+const Button = ({ onClick, text}) => {
     return (
-        <button onClick={props.onClick}>
-            {props.text}
+        <button onClick={onClick}>
+            {text}
         </button>
     )
 }
