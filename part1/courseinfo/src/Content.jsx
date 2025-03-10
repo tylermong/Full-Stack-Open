1a10579a0d4ea044a0d17608c4da8e68e8afcd42
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-09 22:12:30 -0400

    Refactor App, Content, Header, and Total components to use parts array for improved data handling

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,7 +3,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part name={props.course.name} exercises={props.course.exercises} />
+            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
         </>
     )
 }
