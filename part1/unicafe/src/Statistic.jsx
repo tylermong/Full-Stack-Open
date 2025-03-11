1699366e5e12d1fab8e1eb8ef7de2c3b1d787a7b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-11 16:20:50 -0400

    Add average and positive percentage calculations to Statistics component

--- part1/unicafe/src/Statistic.jsx
+++ part1/unicafe/src/Statistic.jsx
@@ -1,10 +1,23 @@
+
+
+const calculateAverage = (props) => {
+    return (good + neutral + bad) / 3
+}
+
 const Statistics = ({ good, neutral, bad }) => {
+    let total = good + neutral + bad
+    let average = (good - bad) / total
+    let positive = (good / total) * 100
+
     return ( 
         <div>
             <h1>statistics</h1>
             <div>good {good}</div>
             <div>neutral {neutral}</div>
             <div>bad {bad}</div>
+            <div>all {total}</div>
+            <div>average {average}</div>
+            <div>positive {positive} %</div>
         </div>
     )
 }
