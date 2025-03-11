abf3c0170f5fccc5c1f86160a66d8494043f7700
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-11 16:38:04 -0400

    Update StatisticsLine component to use table row structure for better layout

--- part1/unicafe/src/StatisticsLine.jsx
+++ part1/unicafe/src/StatisticsLine.jsx
@@ -1,6 +1,9 @@
 const StatisticsLine = ({text, value}) => {
     return (
-        <div>{text} {value}</div>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
