84a6edd7f0a9556c7cf7e66f4042f5c9a7874004
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-11 16:06:07 -0400

    Add feedback buttons and state management to App component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,41 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const addReview = (rating) => {
+  if (rating === good) {
+    setGood(good + 1)
+  }
+
+  if (rating == neutral) {
+    setNeutral(neutral + 1)
+  }
+
+  if (rating == bad) {
+    setBad(bad + 1)
+  }
+}
+
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => addReview(good)} text="good" />
+      <Button onClick={() => addReview(neutral)} text="neutral" />
+      <Button onClick={() => addReview(bad)} text="bad" />
       
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>netural {good}</div>
+      <div>bad {good}</div>
     </div>
   )
 }
