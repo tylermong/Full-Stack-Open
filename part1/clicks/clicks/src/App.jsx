c23929ccd89d13154ac715cf2bbf621aa78995e2
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-11 15:40:55 -0400

    Refactor App component to use Button component for left and right click actions

--- part1/clicks/clicks/src/App.jsx
+++ part1/clicks/clicks/src/App.jsx
@@ -15,6 +15,12 @@ const History = (props) => {
   )
 }
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -38,8 +44,8 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
       {right}
       <History allClicks={allClicks} />
     </div>
