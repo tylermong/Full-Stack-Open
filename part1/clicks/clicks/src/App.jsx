4bcb9cdbb36315f5e6556820f335c696f72329c3
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-11 15:43:00 -0400

    Refactor App component to implement value state management and update button functionalities

--- part1/clicks/clicks/src/App.jsx
+++ part1/clicks/clicks/src/App.jsx
@@ -1,55 +1,26 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
   </button>
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
-
 export default App
