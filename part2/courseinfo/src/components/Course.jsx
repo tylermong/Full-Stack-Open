f70be6222a82240374067b0f691a9f5fa2e826f6
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-04-10 13:49:30 -0400

    refactor Course component to accept props for Header, Content, and Part

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,17 +1,35 @@
-const Header = () => {
-    
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
 }
 
-const Content = () => {
-
+const Content = ({ content }) => {
+  return (
+    <>
+      {content.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  )
 }
 
-const Part = () => {
-
+const Part = ({ id, name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
 }
 
-const Course = () => {
-
+const Course = ({ course }) => {
+  console.log(course)
+  return (
+    <>
+      <Header name={course.name} />
+      <Content content={course.parts} />
+    </>
+  )
 }
 
 export default Course
