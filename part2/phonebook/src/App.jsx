4ed9af4506a7fdb15e3d39265fd991610b060d48
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-18 12:43:51 -0400

    add functionality for user to add names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,18 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name}
+        </div>
+      ))}
     </div>
   );
 };
