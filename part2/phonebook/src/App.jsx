b97d73431535c859129b6f59cd41db0f4c9d4ef9
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-18 12:25:34 -0400

    add provided code

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const App = () => {
-  return "";
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+    </div>
+  );
 };
 
 export default App;
