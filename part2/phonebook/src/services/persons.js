7975d2c749a694d136eed8e9868777788d75f2f6
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-01 10:32:39 -0400

    fix: correct deletePerson function to use DELETE method instead of PUT

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -18,7 +18,7 @@ const update = (id, newObject) => {
 };
 
 const deletePerson = (id) => {
-  const request = axios.put(`${baseUrl}/${id}`);
+  const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => response.data);
 };
 
