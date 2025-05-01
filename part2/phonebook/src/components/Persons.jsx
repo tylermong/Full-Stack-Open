49de62bbd4ddf88d495503906b6273a6fef2f3e3
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-01 10:33:07 -0400

    feat: add deletePerson prop to Persons component for handling deletions

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,9 +1,10 @@
-const Persons = ({ personsToShow }) => {
+const Persons = ({ personsToShow, deletePerson }) => {
   return (
     <>
       {personsToShow.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
+          <button onClick={() => deletePerson(person.id)}>delete</button>
         </div>
       ))}
     </>
