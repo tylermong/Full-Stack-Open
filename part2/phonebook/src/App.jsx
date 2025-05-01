0902d43d359a2ba7dc07b8482860197fb943c99c
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-30 23:05:46 -0400

    refactor: replace axios call with personService for fetching persons

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import axios from "axios";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,9 +11,8 @@ const App = () => {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
